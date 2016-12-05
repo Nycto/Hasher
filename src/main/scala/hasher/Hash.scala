8ff1055bcaa128abbbe69d1fb09b4a8236eddef7
y2k2mt <y2.k2mt@gmail.com>
2016-12-05 23:50:40 +0900

    Apply review results.

--- src/main/scala/hasher/Hash.scala
+++ src/main/scala/hasher/Hash.scala
@@ -16,22 +16,25 @@ object Hash {
 
     /** Implicitly converts from a hash to a byte array */
     implicit def hashToByteArray ( from: Hash ): Array[Byte] = from.bytes
+
+    private[hasher] val HexChars = "0123456789abcdef".toCharArray
 }
+
 
 /**
  * Represents a hash
  */
 case class Hash ( val bytes: Array[Byte] ) extends Equals {
 
+    import Hash._
+
     /** Constructs a hash from a hex string */
     def this ( hex: String ) =
         this( hex.grouped(2).map( Integer.parseInt(_, 16).byteValue ).toArray )
 
-    private[this] val HexChars = "0123456789abcdef".toCharArray
-
     /** Converts this hash to a hex encoded string */
     lazy val hex: String = {
-      val buffer = new StringBuilder
+      val buffer = new StringBuilder(bytes.length * 2)
       bytes.foreach { byte =>
         buffer.append(HexChars((byte & 0xF0) >> 4))
         buffer.append(HexChars(byte & 0x0F))
